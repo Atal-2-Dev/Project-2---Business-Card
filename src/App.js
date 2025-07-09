3ca7b5be4a187aa5bf4176216f08f9f2fb15112c
Atal-2-Dev <a.dev.about@gmail.com>
2025-07-09 20:42:18 +0545

    Create a new project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+import MainComponent from './components/MainComponent';
+
+export default function App (){
+  return (
+    <div>
+      <Navbar/>
+      <MainComponent/>  
+    </div>
+  );
+}
